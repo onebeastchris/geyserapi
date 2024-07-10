94520644bfdd32583dbb1c7a5c882536e08e7d4a
onebeastchris <github@onechris.mozmail.com>
2024-07-10 14:25:19 +0200

    Idea for different approach to version checking

--- base/src/main/java/org/geysermc/api/util/ApiVersion.java
+++ base/src/main/java/org/geysermc/api/util/ApiVersion.java
@@ -4,14 +4,23 @@ package org.geysermc.api.util;
  * Represents a version of the api.
  */
 public class ApiVersion {
+    private final int human;
     private final int major;
     private final int minor;
-    private final int patch;
 
-    public ApiVersion(int major, int minor, int patch) {
-        this.major = major;
-        this.minor = minor;
-        this.patch = patch;
+    public ApiVersion(int human, int minor, int patch) {
+        this.human = human;
+        this.major = minor;
+        this.minor = patch;
+    }
+
+    /**
+     * Returns the human version of the api.
+     *
+     * @return the human version
+     */
+    public int human() {
+        return this.human;
     }
 
     /**
@@ -33,28 +42,62 @@ public class ApiVersion {
     }
 
     /**
-     * Returns the patch version of the api.
+     * Checks whether the requested version is compatible with this version.
+     * The parameters represent the desired api version, which is checked against this version.
+     * The human version must match, and the desired major version must be equal or smaller than this major version.
+     * If the major versions match, then the desired minor version must be equal or smaller than this minor version.
      *
-     * @return the patch version
+     * @param human the desired human version
+     * @param major the desired major version
+     * @param minor the desired minor version
+     * @return true if the requested api version is supported by the present API version
      */
-    public int patch() {
-        return this.patch;
+    public boolean supportsRequestedVersion(int human, int major, int minor) throws IncompatibleVersionException {
+        if (human != this.human) {
+            throw new IncompatibleVersionException(IncompatibleVersionException.Reason.HUMAN_DIFFER);
+        }
+
+        if (major > this.major) {
+            throw new IncompatibleVersionException(IncompatibleVersionException.Reason.MAJOR_DIFFER);
+        }
+
+        if (minor > this.minor && major >= this.major) {
+            throw new IncompatibleVersionException(IncompatibleVersionException.Reason.MINOR_DIFFER);
+        }
+
+        return true;
     }
 
     /**
-     * Checks whether the given version is compatible with this version.
-     * The parameters show the desired API version, which is checked against this present API version.
-     * The major versions must match, and the desired minor version must be equal or smaller than this minor version.
-     * If the minor versions match, then the desired patch version must be equal or smaller than this patch version.
-     *
-     * @param major the desired major version
-     * @param minor the desired minor version
-     * @param patch the desired patch version
-     * @return true if the desired API version is compatible with the present API version
+     * An exception thrown in {@link #supportsRequestedVersion(int, int, int)} when there is a version mismatch.
      */
-    public boolean isCompatible(int major, int minor, int patch) {
-        return major == this.major
-                && minor <= this.minor
-                && (patch <= this.patch || minor < this.minor);
+    public static class IncompatibleVersionException extends Exception {
+        private static final long serialVersionUID = 1L;
+        private final IncompatibleVersionException.Reason reason;
+
+        public IncompatibleVersionException(org.geysermc.api.util.ApiVersion.IncompatibleVersionException.Reason reason) {
+            super(reason.cause());
+            this.reason = reason;
+        }
+
+        public IncompatibleVersionException.Reason reason() {
+            return this.reason;
+        }
+
+        public enum Reason {
+            HUMAN_DIFFER("The human api version does not match!"),
+            MAJOR_DIFFER("The major api version is too high!"),
+            MINOR_DIFFER("The minor api version is too high!");
+
+            private final String cause;
+
+            Reason(String cause) {
+                this.cause = cause;
+            }
+
+            public String cause() {
+                return this.cause;
+            }
+        }
     }
 }
